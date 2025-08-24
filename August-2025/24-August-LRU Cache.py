989feeb7ece8c660d7703fccdaeebc72f59ecd22
pranav-ritvik <157366221+pranav-ritvik@users.noreply.github.com>
2025-08-24 20:16:17 +0530

    (Leet Code) 31st-Day LRU Cache
    Implemented LRU Cache with get and put methods.

---
+++ August-2025/24-August-LRU Cache.py
@@ -0,0 +1,48 @@
+class Node:
+    def __init__(self, key, val):
+        self.key, self.val = key, val
+        self.prev = self.next = None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cap = capacity
+        self.cache = {}  # key -> Node
+
+        # sentinels: left = LRU end, right = MRU end
+        self.left, self.right = Node(0, 0), Node(0, 0)
+        self.left.next, self.right.prev = self.right, self.left
+
+    def remove(self, node):
+        prev, nxt = node.prev, node.next
+        prev.next, nxt.prev = nxt, prev
+
+    def insert(self, node):
+        # insert at MRU end (just before right)
+        prev, nxt = self.right.prev, self.right
+        prev.next = nxt.prev = node
+        node.prev, node.next = prev, nxt
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        node = self.cache[key]
+        self.remove(node)
+        self.insert(node)
+        return node.val
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            node = self.cache[key]
+            node.val = value
+            self.remove(node)
+            self.insert(node)
+        else:
+            node = Node(key, value)
+            self.cache[key] = node
+            self.insert(node)
+
+        if len(self.cache) > self.cap:
+            lru = self.left.next
+            self.remove(lru)
+            del self.cache[lru.key]
