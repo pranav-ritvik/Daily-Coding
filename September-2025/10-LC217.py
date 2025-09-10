04b9e605df8fe268f3a4055ecd5cecca93f1a988
pranav-ritvik <157366221+pranav-ritvik@users.noreply.github.com>
2025-09-10 21:34:06 +0530

    Add containsDuplicate method in Solution class (Day-48)
    Implement containsDuplicate method to check for duplicates in a list.

---
+++ September-2025/10-LC217.py
@@ -0,0 +1,13 @@
+from collections import defaultdict
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        freq = defaultdict(int)
+
+        for num in nums:
+            freq[num] += 1
+        
+        for k,v in freq.items():
+            if v > 1:
+                return True
+            
+        return False
