77fba4fb139f94fb8fb8fd140ce64ede5c7aec6e
pranav-ritvik <157366221+pranav-ritvik@users.noreply.github.com>
2025-09-24 21:42:26 +0530

    Add buildArray method (Day-60)
    Implement buildArray method to construct a new list from nums.

---
+++ September-2025/24-LC1920.py
@@ -0,0 +1,6 @@
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        main_list = []
+        for num in nums:
+            main_list.append(nums[num])
+        return main_list
