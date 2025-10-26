b9933e0a9a4ce27b431c2523b4705f9bd6429233
pranav-ritvik <157366221+pranav-ritvik@users.noreply.github.com>
2025-10-26 12:33:04 +0530

    (Day-86)

---
+++ October-2025/26-LC1.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map= new HashMap<>();
+
+        for(int i=0; i<nums.length; i++){
+            int compliment = target - nums[i];
+            if (map.containsKey(compliment)){
+                int first = map.get(compliment);
+                int second = i;
+                int[] numbers={first,second};
+                return numbers;
+            }
+            map.put(nums[i],i);
+        }
+        return new int[] {};
+    }
+}
