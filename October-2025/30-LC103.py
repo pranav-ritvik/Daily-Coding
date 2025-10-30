43a66fd6fe9e08e1222bac2917d96ebb1aa235ad
pranav-ritvik <157366221+pranav-ritvik@users.noreply.github.com>
2025-10-30 15:51:05 +0530

    (Day-90)

---
+++ October-2025/30-LC103.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import deque
+class Solution:
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        flag = True
+        if not root:
+            return []
+        res = []
+        q = deque([root])
+        while q:
+            level = []
+            for _ in range(len(q)):
+                node = q.popleft()
+                level.append(node.val)
+
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+                
+            if not flag:
+                    level.reverse()
+            res.append(level)
+            flag = not flag
+        return res
