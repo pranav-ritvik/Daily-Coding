3e5feaff4938982ca4b985762eb9041223cdaaed
pranav-ritvik <157366221+pranav-ritvik@users.noreply.github.com>
2025-11-18 21:12:03 +0530

    (Day-105)

--- November-2025/19-Let Me Eat Cake!.py
+++ November-2025/19-Let Me Eat Cake!.py
@@ -1,10 +1,13 @@
-total = 0
-while A != B:
-    if A > B:
-        eat = (A+1)//2
-        A -= eat
-    else:
-        eat = (B+1)//2
-        B -= eat
-    total += eat
-print(total)
+t = int(input())
+for _ in range(t):
+    A, B = map(int, input().split())
+    total = 0
+    while A != B:
+        if A > B:
+            eat = (A + 1) // 2
+            A -= eat
+        else:
+            eat = (B + 1) // 2
+            B -= eat
+        total += eat
+    print(total)
